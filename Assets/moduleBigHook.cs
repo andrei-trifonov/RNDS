e5f2941eedd9aad5c67b8acec89b58bbc71fd25b
trifonov.a <rsanddrs-company@yandex.ru>
2022-10-21 11:47:41 +0300

    BigHook finish
    .

--- Assets/moduleBigHook.cs
+++ Assets/moduleBigHook.cs
@@ -5,19 +5,32 @@ using UnityEngine;
 
 public class moduleBigHook :  commonMagneticPlace{
     // Start is called before the first frame update
+    [SerializeField] private Animator m_Animator;
     private bool isPrepared;
     private bool isGrabbed;
+    private bool isClosed;
     public void Grab()
-    { 
-        //TODO animation
-
+    {
+        if (!isClosed)
+        {
+            m_Animator.SetBool("Catch", true);
+            isClosed = true;
+        }
+        else
+        {
+            m_Animator.SetBool("Catch", false);
+            isClosed = false;
+        }
+        
         if (isPrepared && !isGrabbed)
         {
+            m_Animator.SetBool("Catch", true);
             Picked = true;
             isGrabbed = true;
         }
         else if (isGrabbed)
         {
+            m_Animator.SetBool("Catch", false);
             isGrabbed = false;
             o_MagneticItem = Item.transform.GetChild(0).gameObject.GetComponent<MagneticItem>();
             Picked = false;
